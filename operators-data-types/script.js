658cc2442a957a568e64e228b5b656f55c4abede
Sarah Schoener <sarahv.schoener@gmail.com>
2023-09-20 22:07:25 +0200

    adds operators data type tasks

---
+++ operators-data-types/script.js
@@ -0,0 +1,14 @@
+// 01) Write a String that logs out your first name
+console.log("S" + "a" + "r" + "a" + "h");
+
+// 02) Spot the problems & solve them
+console.log("Hallo");
+console.log("That doesn't work");
+console.log(50 + 2 === 52);
+console.log("333" === 333);
+console.log((600 + 510) / 2);
+console.log(2 + 2);
+
+// 03) Compare two conditions
+console.log(25 / 5 === 5);
+console.log(50 / 10 <= 5 * 2);
